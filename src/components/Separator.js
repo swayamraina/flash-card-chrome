89ef20228be628eb508ee5ce3367b24fd8f6c636
swayamraina <swayamraina@gmail.com>
2020-04-29 00:26:36 +0530

    added separator component and styling

---
+++ src/components/Separator.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../styles/Separator.css'
+
+
+
+function Separator() {
+
+	return (
+		<span className="tag-int-separator"> | </span>
+	);
+	
+}
+
+export default Separator;
