f63edc84f59019035d04f0569b45d138c738dc2c
swayamraina <swayamraina@gmail.com>
2020-04-30 22:32:53 +0530

    added styling to close button component

--- src/components/Close.js
+++ src/components/Close.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import '../styles/Close.css'
 
 
 function Close(props) {
 
 	return (
-		<span>
-			<button onClick={ () => props.onClose(props.index) } > x </button>
+		<span className="cross-hair">
+			<button onClick={ () => props.onClose(props.index) } >X</button>
 		</span>
 	);
 	
