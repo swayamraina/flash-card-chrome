a6a7a08ba80999972638b0b2b2b17b10b2d45845
swayamraina <swayamraina@gmail.com>
2020-05-01 17:27:27 +0530

    styles for close button

--- src/components/Close.js
+++ src/components/Close.js
@@ -6,9 +6,7 @@ import '../styles/Close.css'
 function Close(props) {
 
 	return (
-		<span className="cross-hair">
-			<button onClick={ () => props.onClose(props.index) } >X</button>
-		</span>
+		<button className="cross-hair" onClick={ () => props.onClose(props.index) } >X</button>
 	);
 	
 }
