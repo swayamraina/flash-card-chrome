b677efe90730e242fe7604a8dcf942aff09d49c8
swayamraina <swayamraina@gmail.com>
2020-05-06 22:06:30 +0530

    add save success ccomponent

---
+++ src/components/Success.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import '../styles/Success.css'
+
+
+
+function Success () {
+    return (
+        <div className="success-checkmark">
+            <div className="check-icon">
+                <span className="icon-line line-tip"></span>
+                <span className="icon-line line-long"></span>
+                <div className="icon-circle"></div>
+                <div className="icon-fix"></div>    
+            </div>
+        </div>
+    );
+}
+
+export default Success;
