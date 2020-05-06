b5aace030859ca589b7ab150a8e97d61fe343cb9
swayamraina <swayamraina@gmail.com>
2020-05-06 22:06:45 +0530

    add save failure ccomponent

---
+++ src/components/Failure.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import '../styles/Failure.css'
+
+
+
+function Failure () {
+    return (
+        <div className="swal2-icon swal2-error swal2-animate-error-icon">
+            <span className="swal2-x-mark">
+                <span className="swal2-x-mark-line-left"></span>
+                <span className="swal2-x-mark-line-right"></span>
+            </span>
+        </div>
+    );
+}
+
+export default Failure;
