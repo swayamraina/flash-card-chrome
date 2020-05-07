0bb36e9b285c0561f0573e8c5d8106b15ec8f68e
swayamraina <swayamraina@gmail.com>
2020-05-07 19:46:59 +0530

    added success, failure, loading screen to container

--- src/containers/Container.js
+++ src/containers/Container.js
@@ -2,14 +2,54 @@ import React from 'react';
 
 import Header from '../components/Header'
 import InputContainer from './InputContainer';
+import Success from '../components/Success'
+import Failure from '../components/Failure'
+
+import '../styles/Container.css'
 
 
 
-function Container() {
+function Container (props) {
+	
+	const loadingScreen = () => {
+		return (
+			<div className="lds-ellipsis">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		);
+	}
+
+	const saveResultScreen = () => {
+		return (
+			<div>
+				{ props.success === 0 ? <Failure /> : <Success /> }
+			</div>
+		);
+	}
+
+	const inputScreen = () => {
+		return (
+			<div>
+				<Header />
+				<InputContainer loading={props.loading} setLoading={props.setLoading} setSuccess={props.setSuccess} />
+			</div>
+		);
+	}
+
+	const homeScreen = () => {
+		return (
+			<div>
+				{props.success === -1 ? inputScreen() : saveResultScreen() }
+			</div>
+		);
+	}
+
 	return (
 		<div>
-			<Header />
-			<InputContainer />
+			{ props.loading ? loadingScreen() : homeScreen() }
 		</div>
 	);
 }
